d8618ce74bd45d133c3d68f7ddef3872711ab8c1
Your Name <kinshat1995@gmail.com>
2019-12-16 02:51:49 +0100

    adding click listener to toolbar icon

--- ToolBarDemo/app/src/main/java/com/example/toolbardemo/MainActivity.java
+++ ToolBarDemo/app/src/main/java/com/example/toolbardemo/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.toolbardemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +29,24 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater() ;
         menuInflater.inflate(R.menu.app_menu_file , menu);
         return true ;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        // checking which item was selected..
+        switch (item.getItemId())
+        {
+            case R.id.menu_search :
+                Toast.makeText(this , "Search clicked" , Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_folder :
+                Toast.makeText(this, "folder clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_share :
+                Toast.makeText(this , "share clicked" , Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
