66cded0bb9efa3214d0ac90ae11c2fdc64b1dfd5
Your Name <kinshat1995@gmail.com>
2019-12-18 03:10:11 +0100

    inflating the icon on the toolbar

--- RecyclerFilterApp/app/src/main/java/com/example/recyclerfilterapp/MainActivity.java
+++ RecyclerFilterApp/app/src/main/java/com/example/recyclerfilterapp/MainActivity.java
@@ -1,17 +1,46 @@
 package com.example.recyclerfilterapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolbar  ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolbar = findViewById(R.id.id_tool_bar) ;
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar() ;
+       // actionBar.setDisplayShowHomeEnabled(true);
+
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater() ;
+        menuInflater.inflate(R.menu.menu_tool , menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
