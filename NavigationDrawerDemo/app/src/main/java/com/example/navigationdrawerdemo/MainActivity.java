05eb342c01b2b3fe68693579031eff47833ed4d9
Your Name <kinshat1995@gmail.com>
2019-12-25 14:05:00 +0100

    adding toolbar layou file

--- NavigationDrawerDemo/app/src/main/java/com/example/navigationdrawerdemo/MainActivity.java
+++ NavigationDrawerDemo/app/src/main/java/com/example/navigationdrawerdemo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.navigationdrawerdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolbar ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.id_toolbar);
+        setSupportActionBar(mToolbar);
     }
 }
