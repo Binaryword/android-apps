8ed79f2b53d21bb14a5e67eac245885c32bcb693
Your Name <kinshat1995@gmail.com>
2019-12-30 15:32:10 +0100

    add listerner to home icon

--- NavigationDrawerDemo/app/src/main/java/com/example/navigationdrawerdemo/MainActivity.java
+++ NavigationDrawerDemo/app/src/main/java/com/example/navigationdrawerdemo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
@@ -87,4 +88,31 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext() , message , Toast.LENGTH_SHORT).show();
 
     }// end show message.......
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId())
+        {
+            case  android.R.id.home :
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true ;
+
+        }// end of switch
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
