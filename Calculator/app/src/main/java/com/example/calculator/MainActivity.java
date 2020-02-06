c43afdbae3fda2e136c32e0b2ea25ecf0729534b
Your Name <kinshat1995@gmail.com>
2020-02-06 13:46:28 +0100

    calculator xml (GUI) file

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void activateOperand(View view) {
+
+
+    }
+
+    public void activateOperation(View view) {
+
+
+    }
+
+
+
+    public void activateModification(View view) {
+
+
+    }
+
 }
