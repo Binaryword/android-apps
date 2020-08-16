bbeaeb6e344b5bc9e795a8ca10887f8be4fe81fc
Your Name <kinshat1995@gmail.com>
2020-08-16 12:08:55 +0100

    update on fragment communication

--- FragmentCommunication/app/src/main/java/com/example/fragmentcommunication/MainActivity.java
+++ FragmentCommunication/app/src/main/java/com/example/fragmentcommunication/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.fragmentcommunication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements MessageFragment.OnSendMessageRequest {
+
+    private TextView displayText ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(findViewById(R.id.container_id) !=null){
+
+            if(savedInstanceState  != null)
+                return ;
+
+            FragmentManager fragmentManager = getSupportFragmentManager() ;
+            FragmentTransaction fragTran = fragmentManager.beginTransaction() ;
+            MessageFragment messageFragment = new MessageFragment() ;
+            fragTran.add(R.id.container_id , messageFragment ,null).commit();
+
+
+        }
     }
+
+    @Override
+    public void sendMessage(String message) {
+        displayText = findViewById(R.id.show_message_id);
+        displayText.setText(message);
+    }// end of method
 }
